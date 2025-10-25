e96c08ff98511192a4dbf4dcb0e6c8882eec7e26
neerajkr-coding <neerajkr600@gmail.com>
2025-10-26 02:25:15 +0530

    added aws sdk and disabled CSRF

--- cloudbox/src/main/java/com/application/cloudbox/Controller/FileController.java
+++ cloudbox/src/main/java/com/application/cloudbox/Controller/FileController.java
@@ -1,16 +1,25 @@
 package com.application.cloudbox.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileController {
 
     @GetMapping("test")
-    public String test() {
+    private String test() {
         return "Hello World";
     }
 
-
+    @PostMapping("upload-file")
+    public String uploadFile(
+            @RequestParam("file") MultipartFile file
+            ) {
+        MultipartFile temp = file;
+        return "done";
+    }
 
 }
